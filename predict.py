777a931ba1e0c238c3a868337ae768d0f8542e60
Haldies Gerhardien Pasya <haldiespasya@gmail.com>
2025-06-14 02:19:33 +0700

    add file py

--- predict.py
+++ predict.py
@@ -1,18 +1,41 @@
 import pandas as pd
 import xgboost as xgb
-import sys
 
+# Load model
 model = xgb.XGBClassifier()
 model.load_model("model_xgb.json")
 
+data = pd.DataFrame({
+    'Age': [20],
+    'BusinessTravel': [1],
+    'Department': [0],
+    'DistanceFromHome': [11],
+    'EducationField': [4],
+    'EnvironmentSatisfaction': [3],
+    'Gender': [1],
+    'JobInvolvement': [3],
+    'JobLevel': [2],
+    'JobRole': [4],
+    'JobSatisfaction': [2],
+    'MaritalStatus': [0],
+    'MonthlyIncome': [4777],
+    'OverTime': [0],
+    'StockOptionLevel': [0],
+    'TotalWorkingYears': [15],
+    'YearsAtCompany': [1],
+    'YearsInCurrentRole': [0],
+    'YearsWithCurrManager': [0]
+})
 
-input_file = sys.argv[1]  
-data = pd.read_csv(input_file)
 
-
+# Prediksi kelas (0 atau 1)
 predictions = model.predict(data)
 
-output = pd.DataFrame(predictions, columns=["Prediction"])
-output.to_csv("predictions.csv", index=False)
+# Loop tiap baris hasil prediksi
+for i, pred in enumerate(predictions):
+    if pred == 1:
+        print(f"Attrition")
+    else:
+        print(f"No Attrition")
 
-print("Prediksi selesai! Disimpan di predictions.csv")
+
